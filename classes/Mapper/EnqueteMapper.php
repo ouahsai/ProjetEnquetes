46f0a967e0e89f18de0957e420c23db694262a77
stagiaire <stagiaire@192.168.1.135>
2014-04-30 17:53:38 +0200

    enqueteMapper : ajout de la function getDeleteByIdEnquete Modif membre ajout img png

--- classes/Mapper/EnqueteMapper.php
+++ classes/Mapper/EnqueteMapper.php
@@ -10,14 +10,14 @@ class EnqueteMapper {
         $this->_pdo = \Manager\PDO::pdoConnection();
     }
 
-    public function getEnqueteById($id){
+    public function getEnqueteByIdUtilisateur($id){
              
-        $query = "SELECT titre FROM enquete where id_enquete = :id";
+        $query = "SELECT ID_ENQUETE,TITRE,DESCRIPTION FROM enquete where ID_UTILISATEUR = :id";
         
         $stmt = $this->_pdo->prepare($query);
         $stmt->bindValue(":id", $id);
         $stmt->execute();
-        $listEnquetes = $stmt->fetch(\PDO::FETCH_ASSOC);
+        $listEnquetes = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         
         if ($listEnquetes){
             return $listEnquetes;
@@ -26,4 +26,20 @@ class EnqueteMapper {
             return false;
         }
      }
+     
+    public function deleteEnqueteById($id){
+        $query = "DELETE FROM enquete WHERE ID_ENQUETE = :id";
+        
+        $stmt = $this->_pdo->prepare($query);
+        $stmt->bindValue(":id", $id);
+        $stmt->execute();
+        if ($stmt){
+            $message = "Votre enquête a été supprimée de la base de données";
+            return $message;
+        }
+        else{
+            $message = "Une erreur est survenue dans la suppression de l'enquête, veuillez reessayer";
+            return $message;
+        }
+    }
 }
--- member.php
+++ member.php
@@ -1,12 +1,16 @@
 <?php 
-session_start();
+require_once './includes/autoload.php';
 
+session_start();
 if(isset($_SESSION['user_id'])){
     
     //si la session utilisateur existe, la personne est connecté donc on recupere la liste des enquetes de cette personne
     $enquete = new Mapper\EnqueteMapper();
-    $listEnquetes = $enquete->getEnqueteById($_SESSION['user_id']); 
+    $listEnquetes = $enquete->getEnqueteByIdUtilisateur(1); 
+    var_dump($listEnquetes);
 }
+
+   $message = "Vous n'avez pas encore créé d'enquêtes!";
 ?>
 
 <!DOCTYPE html>
@@ -21,27 +25,34 @@ if(isset($_SESSION['user_id'])){
     <body>
         <div class="container">
             
-            <h4>Compte de PASCAL HENRI</h4>
-            <img src="#" alt="photo profil" title="Photo profil" height="70" width="70">
-            <button>Modifier compte</button>
-            <button>Supprimer compte</button>
+            
+            <img src="img/users_default.png" alt="photo profil" title="Photo profil" height="100" width="100">
+            <!--<h4>Compte de <?php $_SESSION['nom']?> <?php $_SESSION['prenom']?></h4>-->
+            <div>
+                <button>Modifier compte</button><br>
+                <button>Supprimer compte</button>
+            </div>
             
             <h5>Liste des enquetes :</h5>
             <div>
                 <?php if ($listEnquetes){?>
                 <ul>
                    <?php foreach ($listEnquetes as $elt){?>
-                    <li><?php echo $elt ?></li></br>
-                       <button class="btn btn-primary">Show</button>
-                       <button class="btn btn-warning">Update</button>
-                       <button class="btn btn-danger">Delete</button>
-                       <button class="btn btn-success">Results</button>
+                    <li><?php echo $elt['TITRE'] ?></li><br>
+                    <a href="creation_modif.php"><button class="btn btn-primary">Show</button></a>
+                    <a href="creation_modif.php?id=<?php echo $elt['ID_ENQUETE']?>"><button class="btn btn-warning">Update</button></a>
+                    <button class="btn btn-danger">Delete</button>
+                    <a href="result.php"><button class="btn btn-success">Results</button></a>
+                    <br><br>
                     <?php } ?>
                 </ul>
-                <?php } ?>                 
+                
+                <?php } else{ ?>
+                <p><?php echo $message; ?></p>
+                <?php }?>
             </div>  
             <div>
-                <a href="creation_modification.php"><button class="btn btn-default">Nouvelle Enquete</button></a>
+                <a href="creation_modif.php"><button class="btn btn-default">Nouvelle Enquete</button></a>
             </div>
             </div>
     </body>
