c3405cc22813b49e44974324fa025371d64439c7
Gautier <Gautier@Gautier-VAIO>
2014-05-02 17:37:12 +0200

    change Reponse Class properties

--- classes/Entity/Reponse.php
+++ classes/Entity/Reponse.php
@@ -2,53 +2,52 @@
 
 namespace Entity;
 
-class Reponse {
-    
-    protected $idReponse;
-    protected $valeurReponse;
-    protected $uniqueUserId;
+class Reponse
+{
+    protected $id_reponse;
+    protected $valeur_reponse;
+    protected $unique_user_id;
     
     /**
-     * 
+     * Association avec question
      * @var Question
      */
-    protected $idQuestion;
+    protected $id_question;
     
-    public function getIdReponse() {
-        return $this->idReponse;
+        
+    public function getId_reponse() {
+        return $this->id_reponse;
     }
 
-    public function getValeurReponse() {
-        return $this->valeurReponse;
+    public function getValeur_reponse() {
+        return $this->valeur_reponse;
     }
 
-    public function getUniqueUserId() {
-        return $this->uniqueUserId;
+    public function getUnique_user_id() {
+        return $this->unique_user_id;
     }
 
-    public function setIdReponse($idReponse) {
-        $this->idReponse = $idReponse;
+    public function getId_question() {
+        return $this->id_question;
+    }
+
+    public function setId_reponse($id_reponse) {
+        $this->id_reponse = (int) $id_reponse;
         return $this;
     }
 
-    public function setValeurReponse($valeurReponse) {
-        $this->valeurReponse = $valeurReponse;
+    public function setValeur_reponse($valeur_reponse) {
+        $this->valeur_reponse = (string) $valeur_reponse;
         return $this;
     }
 
-    public function setUniqueUserId($uniqueUserId) {
-        $this->uniqueUserId = $uniqueUserId;
-        return $this;
-    }
-    public function getIdQuestion() {
-        return $this->idQuestion;
-    }
-
-    public function setIdQuestion(Question $idQuestion) {
-        $this->idQuestion = $idQuestion;
+    public function setUnique_user_id($unique_user_id) {
+        $this->unique_user_id = (string) $unique_user_id;
         return $this;
     }
 
-
-
+    public function setId_question(Question $id_question) {
+        $this->id_question = (int) $id_question;
+        return $this;
+    }
 }
--- classes/Mapper/EnqueteMapper.php
+++ classes/Mapper/EnqueteMapper.php
@@ -2,30 +2,53 @@
 
 namespace Mapper;
 
-class EnqueteMapper {
-
+class EnqueteMapper 
+{
     protected $_pdo;
     
-    
-    public function __construct() {
+    public function __construct() 
+    {
         $this->_pdo = \Manager\PDO::pdoConnection();
     }
 
-    public function getEnqueteByIdUtilisateur(\Entity\Enquete $enquete, \Entity\Pagination $pagination){
-             
-        $query ="SELECT COUNT(*) as nb_elt FROM enquete WHERE ID_UTILISATEUR = :id";
+    public function add(\Entity\Enquete $enquete) 
+    {
+        $query = "INSERT INTO enquete (id_utilisateur, titre, description)
+                  VALUES (:id_utilisateur, :titre, :description)";
+        
+        $stmt = $this->_pdo->prepare($query);
+        
+        $stmt->bindValue("id_utilisateur", $enquete->getId_utilisateur());
+        $stmt->bindValue("titre", $enquete->getTitre());
+        $stmt->bindValue("description", $enquete->getDescription());
+        
+        $succes = $stmt->execute();
+        
+        if(!$succes) {
+            return false;
+        }
+        
+        return $this->_pdo->lastInsertId();
+    }
+    
+    public function getEnqueteByIdUtilisateur(\Entity\Enquete $enquete, \Entity\Pagination $pagination)
+    {  
+        $query = "SELECT COUNT(*) as nb_elt
+                  FROM enquete 
+                  WHERE ID_UTILISATEUR = :id";
+        
         $stmt = $this->_pdo->prepare($query);
         $stmt->bindValue(":id", $enquete->getUtilisateur()->getId_utilisateur());
         $stmt->execute();
         $nb_elt = $stmt->fetch(\PDO::FETCH_ASSOC)['nb_elt'];
         
-        
         $nb_Query = $pagination->set_number_pages($nb_elt);
         $pageDebut= $pagination->get_display_pages();
         $pagefin = $pagination->get_page_fin();
         
-        
-        $query1 = "SELECT ID_ENQUETE,TITRE,DESCRIPTION FROM enquete WHERE ID_UTILISATEUR = :id LIMIT $pageDebut,$pagefin";
+        $query1 = "SELECT ID_ENQUETE,TITRE,DESCRIPTION
+                   FROM enquete
+                   WHERE ID_UTILISATEUR = :id LIMIT $pageDebut,$pagefin";
         
         $stmt = $this->_pdo->prepare($query1);
         $stmt->bindValue(":id", $enquete->getUtilisateur()->getId_utilisateur());
@@ -42,23 +65,23 @@ class EnqueteMapper {
         return $this->_pdo->lastInsertId();
     }
     
-    public function getEnqueteByIdUtilisateur(\Entity\Enquete $enquete) 
-    {
-        $query = "SELECT id_enquete, titre, description
-                  FROM enquete 
-                  WHERE id_utilisateur = :id";
-
-        $stmt = $this->_pdo->prepare($query);
-        $stmt->bindValue(":id", $enquete->getId_utilisateur());
-        $stmt->execute();
-        $listEnquetes = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-
-        if ($listEnquetes) {
-            return $listEnquetes;
-        } else {
-            return false;
-        }
-    }
+//    public function getEnqueteByIdUtilisateur(\Entity\Enquete $enquete) 
+//    {
+//        $query = "SELECT id_enquete, titre, description
+//                  FROM enquete 
+//                  WHERE id_utilisateur = :id";
+//
+//        $stmt = $this->_pdo->prepare($query);
+//        $stmt->bindValue(":id", $enquete->getId_utilisateur());
+//        $stmt->execute();
+//        $listEnquetes = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+//
+//        if ($listEnquetes) {
+//            return $listEnquetes;
+//        } else {
+//            return false;
+//        }
+//    }
 
     public function deleteEnqueteById(\Entity\Enquete $enquete) 
     {
