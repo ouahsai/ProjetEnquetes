bc7096c15eb5a7e48d912f699d08cf36902e3872
JUJki <julien.janneau@gmail.com>
2014-05-05 17:24:03 +0200

    Ajout de requete de resulat dans reponsemapper Ajout d'une protected dans Enquete.PHP $liste_type_question Ajout du delete utilisateur dans le mapper utilisateur

--- classes/Mapper/ReponseMapper.php
+++ classes/Mapper/ReponseMapper.php
@@ -34,10 +34,11 @@ class ReponseMapper
     
     public function totalReponseByIdEnquete(\Entity\Reponse $reponse)
     {
-        $query = "SELECT count(r.unique_user_id)as nb_reponse FROM reponse r
+        $query = "SELECT count(r.unique_user_id)as nb_reponse
+                  FROM reponse r
                   inner join question q on q.id_question = r.id_question
                   inner join enquete e on e.id_enquete = q.id_enquete
-                  where q.id_enquete = :id_enquete";
+                  WHERE q.id_enquete = :id_enquete";
         
         $stmt = $this->_pdo->prepare($query);
         
@@ -54,19 +55,20 @@ class ReponseMapper
     
     public function reponseByIdTypeQuestion(\Entity\Reponse $reponse)
     {
-        $query = "SELECT r.valeur_reponse
-                 FROM reponse r
-                 INNER JOIN question q ON q.id_question = r.id_question
-                 INNER JOIN enquete e ON e.id_enquete = q.id_enquete
-                 INNER JOIN type_question t ON t.id_type_question = q.id_type_question
-                 WHERE q.id_question = :id_question
-                 AND q.id_enquete = :id_enquete
-                 AND t.libelle_type_question = \"Texte\"";
+        $query = "SELECT q.libelle_question, r.valeur_reponse
+                  FROM reponse r
+                  INNER JOIN question q ON q.id_question = r.id_question
+                  INNER JOIN enquete e ON e.id_enquete = q.id_enquete
+                  INNER JOIN type_question t ON t.id_type_question = q.id_type_question
+                  WHERE q.id_question = :id_question
+                  AND q.id_enquete = :id_enquete
+                  AND t.libelle_type_question = :libelle_type_question";
               
         $stmt = $this->_pdo->prepare($query);
-        
-        $stmt->bindValue(":id_type_question", $reponse->getId_type_question());
-        $stmt->bindValue("::id_enquete", $reponse->getId_enquete());
+            
+        $stmt->bindValue(":id_question", $reponse->getId_question());
+        $stmt->bindValue(":id_enquete", $reponse->getId_enquete());
+        $stmt->bindValue(":libelle_type_question", $reponse->getId_enquete());
 
         $succes = $stmt->execute();
         
